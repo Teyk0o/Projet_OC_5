2987994bee44cae4f56bc5d78f1e8de1bc80f798
Théo Vilain <theovilainfr@gmail.com>
2023-09-16 11:31:09 +0200

    docs: fixed SQL schema

--- sql/sql_schema.sql
+++ sql/sql_schema.sql
@@ -1,17 +1,17 @@
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL, -- stocké sous forme hachée
-    email VARCHAR(255) UNIQUE NOT NULL,
+    username LONGTEXT NOT NULL,
+    password LONGTEXT NOT NULL,
+    email LONGTEXT UNIQUE NOT NULL,
     role ENUM('admin', 'user') DEFAULT 'user',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE posts (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    content TEXT NOT NULL,
-    chapo TEXT,
+    title LONGTEXT NOT NULL,
+    content LONGTEXT NOT NULL,
+    chapo LONGTEXT,
     author_id INT,
     last_modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (author_id) REFERENCES users(id)
@@ -19,7 +19,7 @@ CREATE TABLE posts (
 
 CREATE TABLE comments (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    content TEXT NOT NULL,
+    content LONGTEXT NOT NULL,
     author_id INT,
     post_id INT,
     approved ENUM('yes', 'no') DEFAULT 'no',
